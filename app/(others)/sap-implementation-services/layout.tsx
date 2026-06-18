73aeb80658917bee8e5047c8f58ac79e23be2f7e
Shubham Soni <dev.shubhamsoni@gmail.com>
2026-06-18 18:04:23 +0530

    changes

---
+++ app/(others)/sap-implementation-services/layout.tsx
@@ -0,0 +1,20 @@
+import { FooterMain } from "@/components/navigation/footer/footer-main";
+import { Header } from "@/components/navigation/navigation/header";
+
+
+export default function LayoutKyc({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+
+  return (
+    <section className="min-h-screen">
+   
+      <Header />
+      {children}
+      <FooterMain />
+    </section>
+  );
+}
